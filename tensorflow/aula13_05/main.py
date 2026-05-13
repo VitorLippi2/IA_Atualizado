04ba363bb1c0672dd5c0071aaa4c7d758524bd31
VitorLippi2 <vitor.lippi@viannasempre.com.br>
2026-05-13 20:19:03 -0300

    1305

---
+++ tensorflow/aula13_05/main.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+url = "tensorflow/auto-mpg/auto-mpg.data"
+
+columns = ['MPG', 'Cylinders', 'Displacement', 'Horsepower', 'Weight',
+                'Acceleration', 'Model Year', 'Origin']
+raw_dataset = pd.read_csv(
+    url, 
+    names=columns,
+    na_values='?', 
+    comment='\t',
+    sep=r'\s+', 
+    skipinitialspace=True
+)
+
+print(raw_dataset.info())
+
+dataset = raw_dataset.copy()
+dataset = dataset.dropna()
+
+origin = dataset.drop("Origin")
+dataset["USA"] = (origin == 1) * 1.0
+dataset["Europe"] = (origin == 2) * 1.0
+dataset["Japan"] = (origin == 3) * 1.0
+
+print(dataset)
